5afda1a87fd83b74f7bf5b0a2d277560bd80e105
Rafiur Rabby <rrafiurr@gmail.co>
2020-04-10 02:16:26 +0600

    fix problem

---
+++ app/Events/NewMessage.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Events;
+use App\Message;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class NewMessage implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $message;
+
+    public function __construct(Message $message)
+    {
+        $this->message = $message;
+    }
+
+    public function broadcastOn()
+    {
+        return new PrivateChannel('messages.'.$this->message->to);
+    }
+
+    public function broadcastWith()
+    {
+        $this->message->load('fromContact');
+        return ["message" => $this->message];
+    }
+
+}
